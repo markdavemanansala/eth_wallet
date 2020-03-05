52fa08a79394f288c4b5438752becdae0c0a988f
The Blockchain Pundit <davegala09@gmail.com>
2020-03-05 10:58:09 +0800

    Updates

---
+++ precache-manifest.cd2522a6cd58cfcd9634ed8d1891bba3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "10085af7da31ebb80091ec5679356c4b",
+    "url": "/eth_wallet/static/media/tbplogo.10085af7.png"
+  },
+  {
+    "revision": "b53dc6ce415c9777008e",
+    "url": "/eth_wallet/static/js/runtime~main.b53dc6ce.js"
+  },
+  {
+    "revision": "9f339437c79322d236ff",
+    "url": "/eth_wallet/static/js/main.9f339437.chunk.js"
+  },
+  {
+    "revision": "a22f52091497fd5148f9",
+    "url": "/eth_wallet/static/js/1.a22f5209.chunk.js"
+  },
+  {
+    "revision": "9f339437c79322d236ff",
+    "url": "/eth_wallet/static/css/main.6e1577b1.chunk.css"
+  },
+  {
+    "revision": "a22f52091497fd5148f9",
+    "url": "/eth_wallet/static/css/1.f4a7c808.chunk.css"
+  },
+  {
+    "revision": "1aed2a8fbe0ac334eb186a693de8497e",
+    "url": "/eth_wallet/index.html"
+  }
+];
